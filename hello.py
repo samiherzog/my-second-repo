b56decdd7a0c1c959b98bbc53bef8e2464a62b53
Sydney <sydney@0587391322.wireless.umich.net>
2018-01-30 09:45:12 -0500

    favorites

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 print("My name is Sydney")
 print("My name is Sami")
 print("I was born in September")
+
+print("Sydney's favorite food: Mashed potatoes")
+print("Sydney's favorite music: Pop")
+print("Sydney's astrologicl sign: Libra")
