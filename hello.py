065f16a721d8920c94007351c8b8042e4b5403d7
srp250 <35808606+srp250@users.noreply.github.com>
2018-01-30 09:50:33 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ print('Samis favorite food is sushi')
 print('Samis favorite music is pop')
 print('Samis astrological sign is Libra')
 print('Samis SSN is good1')
+
+
+print("Sydney's favorite food: Mashed potatoes")
+print("Sydney's favorite music: Pop")
+print("Sydney's astrologicl sign: Libra")
